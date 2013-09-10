714bd6ccb3e6ab89b82bbf4af7f9f27035317de6
jcollyer <collyerdesign@gmail.com>
2013-09-09 19:59:15 -0700

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Natesite::Application.config.secret_key_base = '72a644d9423b33d5d1929d2e2ca7dad5f45b6ace97e9447d5734dc34da50a1a64e8a4a75ecb5a0138db360a3cfb991baf7556350029d594385a3f8854fc28d35'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Natesite::Application.config.session_store :cookie_store, key: '_natesite_session'
